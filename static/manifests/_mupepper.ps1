358f3848a68ee2e0bde41e7ac5edd9cef644dfdb
Shawn Pearson <1157wcl@gmail.com>
2023-10-30 22:42:14 -0400

    tweak output

--- static/manifests/_mupepper.ps1
+++ static/manifests/_mupepper.ps1
@@ -1155,6 +1155,61 @@ function Remove-AWSCluster {
     if (-not $bypass) { Add-AWSSteps }
 }
 
+# GCP MU Functions
+function add-GCPMultiUserSteps() {
+    # User Options
+    $existingUsers = Send-Update -c "Get Attendees" -r "az ad group member list --group Attendees" | Convertfrom-Json
+    Add-Choice -k "AZMCU" -d "Create Attendee Accounts" -f Add-AzureMultiUser  -c "Current users: $($existingUsers.count)"
+ 
+    #existingUsers fields: displayName, id, userPrincipalName
+    if ($existingUsers.count -gt 0) {
+        Add-Choice -k "AZMDL" -d "  List current Attendee Accounts" -f Get-AzureMultiUser 
+        Add-Choice -k "AZMDU" -d "  Remove Attendee Accounts" -f Remove-AzureMultiUser
+    }
+    else {
+        # End here, no existing users
+        return
+    }
+
+    # Check status for users
+    $parallelResults = [System.Collections.Concurrent.ConcurrentBag[object]]::new()
+ 
+    #   Save functions to string to use in parallel processing
+    $GetUsernameDef = ${function:Get-UserName}.ToString()
+    $SendUpdateDef = ${function:Send-Update}.ToString()
+    $existingUsers | ForEach-Object -Parallel {
+        # Import functions and variables from main script
+        if ($using:showCommands) { $script:showCommands = $true }
+        $script:outputLevel = $using:outputLevel
+        ${function:Get-UserName} = $using:GetUsernameDef
+        ${function:Send-Update} = $using:SendUpdateDef
+        $user = $_
+        $dict = $using:parallelResults
+
+        # Check for resource group
+        $targetGroup = "scw-group-$($user.DisplayName)"
+        $targetCluster = "scw-AKS-$($userProperties.userid)"
+        $groupExists = Send-Update -t 0 -content "Azure: Resource group exists?" -run "az group exists -g $targetGroup"
+        if ($groupExists -eq "true") { $groupName = $true } else { $groupname = $false }
+        $result = New-Object PSCustomObject -Property @{
+            userName      = $user.DisplayName
+            targetGroup   = $targetGroup
+            targetCluster = $targetCluster
+            groupExists   = $groupName
+            clusterExists = $false
+
+        }
+        $dict.add($result)
+    }
+    $hasResourceGroup = $parallelResults | where-object { $_.groupExists }
+    if ($parallelResults.count - $hasResourceGroup.count -ne 0) {
+        $resourceGroups = $parallelResults | where-object { -not $_.groupExists } | select-object -ExpandProperty targetGroup
+        $resourceGroups.count
+        Add-Choice -k "AZMCRG" -d "Create Resource Groups" -c "$($hasResourceGRoup.count)/$($parallelResults.count) created" -f "Add-AzureMultiUserRG -resourceGroups $resourceGroups"
+        return
+    }
+}
+
 # GCP Functions
 function Add-GCPSteps() {
     # Add GCP specific steps
@@ -1178,6 +1233,12 @@ function Add-GCPSteps() {
         Add-Choice -k "GPROJ" -d "Required: Select GCP Project" -f "Set-GCPProject"
         return
     }
+    # -> jump to multi-user mode if selected
+    if ($multiUserMode) {
+        add-GCPMultiUserSteps
+        return
+    }
+
     # We have a valid project, is there a GCP cluster running?
     $gkeClusterName = "scw-gke-$($userProperties.userid)"
     $existingCluster = Send-Update -t 1 -c "Check for existing cluster" -r "gcloud container clusters list --filter=name=$gkeClusterName --format='json' | Convertfrom-Json"
